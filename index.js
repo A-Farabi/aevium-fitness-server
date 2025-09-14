da77b400b612bad5fe37416f1a1702d201a95d1c
A-Farabi <farabisyl2017@gmail.com>
2025-09-15 03:31:05 +0600

    getting data from db: all trainers, speficic trainer

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ require("dotenv").config();
 
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${
-  process.env.DB_PASS
-}@cluster0.mrtaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrtaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,6 +22,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("Aevium_DB");
+    const trainerCollection = database.collection("allTrainers");
+    // General Apis ................................... General Apis
+    app.get("/trainers", async (req, res) => {
+      const result = await trainerCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/trainers/:id', async(req, res)=>{
+      const id = req.params.id
+      try{
+        const trainer =await database.collection("allTrainers").findOne({_id: new ObjectId(id)})
+        if (!trainer) {
+          res.status(404).send({message: "Trainer not found"})
+        }
+        res.send(trainer)
+      }
+      catch(error){
+        res.send(500).send({message: error})
+      }
+    })
+    
+    // General Apis ................................... General Apis
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
